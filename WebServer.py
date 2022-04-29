779d642fcc4606489785c97956facdba82f0b533
Gal <koazgal@gmail.com>
2022-04-29 15:47:03 +0300

    Done

--- WebServer.py
+++ WebServer.py
@@ -1,41 +1,29 @@
-# import socket module
 from socket import *
-import sys  # In order to terminate the program
-
+import sys
 serverSocket = socket(AF_INET, SOCK_STREAM)
-# Prepare a sever socket
-# Fill in start
 serverPort = 6789
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
-# Fill in end
 while True:
-    # Establish the connection
     print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept()  # Fill in start              #Fill in end
+    connectionSocket, addr = serverSocket.accept()
     try:
-        message = connectionSocket.recv(1024)  # Fill in start          #Fill in end
+        message = connectionSocket.recv(1024)
+        print(message)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = f.read()  # Fill in start       #Fill in end
-        # Send one HTTP header line into socket
-        # Fill in start
-        header = "HTTP/1.1 200 OK"
-        # Fill in end
-        # Send the content of the requested file to the client
+        outputdata = f.read()
+        header = "HTTP/1.1 200 OK\r\n\r\n"
+        connectionSocket.send(header.encode())
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode(header))
-
+        connectionSocket.send("\r\n".encode())
+        f.close()
         connectionSocket.close()
     except IOError:
-        # Send response message for file not found
-        # Fill in start
-        connectionSocket.send(str.encode("404 Not Found"))
-        # Fill in end
-        # Close client socket
-        # Fill in start
+        message = "HTTP/1.1 404 Not Found\r\n\r\n"
+        connectionSocket.send(message.encode())
         connectionSocket.close()
-        # Fill in end
-    serverSocket.close()
-    sys.exit()  # Terminate the program after sending the corresponding data
+        break
+serverSocket.close()
+sys.exit()
